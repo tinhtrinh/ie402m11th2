444f7bd511b46184a8b89505effc169fc6f43466
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2021-12-07 13:46:47 +0700

    Toi added c3, c6

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ import { f2base, f2roof, f2floor, f2wall, f2line } from "./f2.js";
 import { f1f2base, f1f2roof, f1f2floor, f1f2wall, f1f2line } from "./f1f2.js";
 
 import { c4base, c4roof, c4floor, c4wall, c4line } from "./c4.js";
+import { c3base, c3roof, c3floor, c3wall, c3line } from "./c3.js";
+import { c6base, c6roof, c6floor, c6wall, c6line } from "./c6.js";
 import { d6base, d6roof, d6floor, d6wall, d6line } from "./d6.js";
 require([
   "esri/Map",
@@ -315,6 +317,87 @@ require([
     });
     graphicsLayer.add(c4lineGraphic);
   }
+
+  /***************************
+     * Add C3
+     ***************************/
+
+   const c3baseGraphic = new Graphic({
+      geometry: c3base,
+      symbol: e1wallSymbol
+  });
+
+  const c3roofGraphic = new Graphic({
+      geometry: c3roof,
+      symbol: e1wallSymbol
+  });
+
+  graphicsLayer.addMany([c3baseGraphic, c3roofGraphic]);
+
+  for(let i = 0; i <= 19; i ++) {
+      let c3floorGraphic = new Graphic({
+          geometry: c3floor[i],
+          symbol: e1floorSymbol
+      });
+      graphicsLayer.add(c3floorGraphic);
+  }
+
+  for(let i = 0; i <= 19; i ++) {
+      let c3wallGraphic = new Graphic({
+          geometry: c3wall[i],
+          symbol: e1wallSymbol
+      });
+      graphicsLayer.add(c3wallGraphic);
+  }
+
+  for(let i = 0; i <= 8; i++) {
+      let c3lineGraphic = new Graphic({
+          geometry: c3line[i],
+          symbol: e1lineSymbol
+      });
+      graphicsLayer.add(c3lineGraphic)
+  }
+
+/***************************
+ * Add C6
+ ***************************/
+
+ const c6baseGraphic = new Graphic({
+      geometry: c6base,
+      symbol: e1wallSymbol
+  });
+
+  const c6roofGraphic = new Graphic({
+      geometry: c6roof,
+      symbol: e1wallSymbol
+  });
+
+  graphicsLayer.addMany([c6baseGraphic, c6roofGraphic]);
+
+  for(let i = 0; i <= 19; i ++) {
+      let c6floorGraphic = new Graphic({
+          geometry: c6floor[i],
+          symbol: e1floorSymbol
+      });
+      graphicsLayer.add(c6floorGraphic);
+  }
+
+  for(let i = 0; i <= 19; i ++) {
+      let c6wallGraphic = new Graphic({
+          geometry: c6wall[i],
+          symbol: e1wallSymbol
+      });
+      graphicsLayer.add(c6wallGraphic);
+  }
+
+  for(let i = 0; i <= 8; i++) {
+      let c6lineGraphic = new Graphic({
+          geometry: c6line[i],
+          symbol: e1lineSymbol
+      });
+      graphicsLayer.add(c6lineGraphic)
+  }
+
 
   /***************************
    * Add D6
